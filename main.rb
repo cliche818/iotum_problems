63d33fa30e77f66daab94e4f90efefb5109424f0
cliche818 <jeff.chun.yinli@gmail.com>
2020-03-22 16:59:06 -0400

    completed question 1

--- main.rb
+++ main.rb
@@ -1,4 +1,20 @@
 require 'pry'
 
 class Main
+  def self.question_1
+    (1..100).each do |i|
+      if i % 3 == 0 && i % 5 == 0
+        p 'FooBar'
+      elsif i % 3 == 0
+        p 'Foo'
+      elsif i % 5 == 0
+        p 'Bar'
+      else
+        p i
+      end
+    end
+  end
 end
+
+
+Main.question_1
